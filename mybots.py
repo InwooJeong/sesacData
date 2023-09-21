0b59973288498202d8e55d1c8ecfeea113d02892
inu <sinth92@hotmail.com>
2023-09-21 17:27:45 +0900

    mybots exam

--- mybots.py
+++ mybots.py
@@ -1,13 +1,11 @@
 import scrapy
-
 from navernews.items import NavernewsItem
-
 
 class MybotsSpider(scrapy.Spider):
     name = "mybots"
     allowed_domains = ["news.naver.com"]
+    # start_urls = ["https://news.naver.com/main/list.naver?mode=LS2D&sid2=262&mid=shm&sid1=101&page=%d" % i for i in range (1,10)]
     start_urls = ["https://news.naver.com/main/list.naver?mode=LS2D&sid2=262&mid=shm&sid1=101"]
-    # start_urls = ["https://news.naver.com/main/list.naver?mode=LS2D&sid2=262&mid=shm&sid1=101&page=%d"%i for i in range(1,10)]
 
     def parse(self, response):
         titles = response.xpath('//*[@id="main_content"]/div[2]/ul/li/dl/dt[2]/a/text()').extract()
@@ -31,6 +29,7 @@ class MybotsSpider(scrapy.Spider):
             
             yield item
             
-        next_page = response.css('.paing a::attr(href)').extract_first()
+        next_page = response.css('.paging a::attr(href)').extract_first()
         if next_page:
-            yield response.follow(next_page, callable=self.parse)
+            yield response.follow(next_page, callback=self.parse)
+            
